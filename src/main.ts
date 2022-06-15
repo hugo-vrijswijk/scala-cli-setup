dff19172e72dab500af2dbf0959cef25ee029ccc
Łukasz Wroński <46607934+lwronski@users.noreply.github.com>
2022-06-15 11:16:20 +0200

    Update Scala CLI to 0.1.8 (#28)

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as path from 'path'
 import * as tc from '@actions/tool-cache'
 
 const csVersion = '2.1.0-M5'
-const scalaCLIVersion = '0.1.7'
+const scalaCLIVersion = '0.1.8'
 const coursierVersionSpec = csVersion
 
 async function execOutput(cmd: string, ...args: string[]): Promise<string> {
