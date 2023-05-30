2d399c8e80b506dacf7e4914e00c23c5609331a9
Piotr Chabelski <ged.subfan@gmail.com>
2023-05-30 13:51:18 +0200

    Bump `coursier` to `2.1.4` (#269)

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as path from 'path'
 import * as tc from '@actions/tool-cache'
 
 let csVersion = core.getInput('version')
-if (!csVersion) csVersion = '2.1.3'
+if (!csVersion) csVersion = '2.1.4'
 const scalaCLIVersion = '1.0.0'
 
 const coursierVersionSpec = csVersion
